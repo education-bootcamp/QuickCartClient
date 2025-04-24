5460c3d8ac50832095a0113efb4a97fede817953
education-bootcamp <education.seekerscloud@gmail.com>
2025-04-24 21:55:07 +0530

    2

--- src/app/modules/security/components/security-footer/security-footer.component.ts
+++ src/app/modules/security/components/security-footer/security-footer.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-security-footer',
   imports: [],
-  standalone:true,
+  standalone: true,
   templateUrl: './security-footer.component.html',
   styleUrl: './security-footer.component.scss'
 })
 export class SecurityFooterComponent {
-
+  year = new Date().getFullYear();
 }
